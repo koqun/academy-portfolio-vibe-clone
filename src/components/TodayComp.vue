d2b70086e6b18fd367f981adb0d44463b4c49acb
jagyeong gu <plzkoiu0912@naver.com>
2022-06-20 09:25:26 +0900

    init: init

--- src/components/TodayComp.vue
+++ src/components/TodayComp.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="today">
-    <h2 class="title">매거진</h2>
     <div class="magazine">
+      <h2 class="title">매거진</h2>
       <div class="mcontainer" v-for="(m, i) in m" :key="i">
         <img class="mimg" :src="require(`@/assets/magazine/${m.img}`)" /><br />
         <a class="mt" :href="`${m.link}`">{{ m.title }}</a
@@ -10,8 +10,8 @@
       </div>
     </div>
 
-    <h2 class="title">뉴스</h2>
     <div class="news">
+      <h2 class="title">뉴스</h2>
       <div class="ncontainer" v-for="(n, i) in n" :key="i">
         <!-- <h2>{{item.img}}</h2> -->
         <img class="nimg" :src="require(`@/assets/news/${n.img}`)" /><br />
@@ -41,15 +41,15 @@ body::-webkit-scrollbar {
 }
 
 .today {
-  left: 0%;
   width: 100%;
   height: 100%;
+  padding-left: 190px;
 }
 
 .magazine {
-  top: 0%;
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
+  height: 50%;
   .mcontainer {
     width: 300px;
     height: 300px;
@@ -62,8 +62,9 @@ body::-webkit-scrollbar {
 }
 
 .news {
-  top: 100%;
   display: flex;
+  flex-wrap: nowrap;
+
   .ncontainer {
     width: 300px;
     height: 200px;
